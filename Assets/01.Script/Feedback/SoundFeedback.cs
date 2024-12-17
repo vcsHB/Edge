eae4d9d962a18a6109f102be0ad5e9a28e5effdd
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-12-18 08:59:01 +0900

    feat: Feedback System, SoundSystem, UtilManager, PoolManager

---
+++ Assets/01.Script/Feedback/SoundFeedback.cs
@@ -0,0 +1,23 @@
+using ObjectPooling;
+using SoundManage;
+using UnityEngine;
+
+namespace FeedbackSystem
+{
+
+    public class SoundFeedback : Feedback
+    {
+
+        [SerializeField] private SoundSO _soundSO;
+
+        public override void CreateFeedback()
+        {
+            SoundPlayer soundPlayer = PoolManager.Instance.Pop(PoolingType.SoundPlayer) as SoundPlayer;
+            soundPlayer.PlaySound(_soundSO);
+        }
+
+        public override void FinishFeedback()
+        {
+        }
+    }
+}
