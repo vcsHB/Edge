9e90d97fe590f349ac83955f57c4b1cd45cb21b6
milkywaycookie <123565749+milkywaycookie@users.noreply.github.com>
2024-12-19 01:12:24 +0900

    fix
    wavemanager

--- Assets/01.Script/WaveSystem/Incomplete&Test/WHTestEnemy.cs
+++ Assets/01.Script/WaveSystem/Incomplete&Test/WHTestEnemy.cs
@@ -2,23 +2,30 @@ using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
 namespace WaveSystem
 {
     public class WHTestEnemy : MonoBehaviour
     {
-        public event Action<WHTestEnemy> OnDieEvent;
-        void Start()
-        {
-            StartCoroutine(PlayerChycle());
-        }
-
-        private IEnumerator PlayerChycle()
-        {
-            yield return new WaitForSeconds(10f);
-            OnDieEvent?.Invoke(this);
-            Destroy(gameObject);
-        }
+        //public event Action<WHTestEnemy> OnDieEvent;
+        //void Start()
+        //{
+        //    StartCoroutine(PlayerChycle());
+        //}
+        //private IEnumerator PlayerChycle()
+        //{
+        //    yield return new WaitForSeconds(10f);
+        //    OnDieEvent?.Invoke(this);
+        //    Destroy(gameObject);
+        //}
+        //private void Update()
+        //{
+        //    if (Input.GetKeyDown(KeyCode.K))
+        //    {
+        //        ScoreManager.Instance.PlusScore(1000);
+        //    }
+        //}
     }
 }
 
