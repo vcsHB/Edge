c29e9f68830de9be00b056cdde6d31c72894de7a
milkywaycookie <123565749+milkywaycookie@users.noreply.github.com>
2024-12-18 12:42:24 +0900

    feat
    WaveSO
    WaveManager제작(미완)

---
+++ Assets/01.Script/WH_Scrips/WaveSO.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace WaveSystem
+{
+
+    [CreateAssetMenu(fileName = "WaveSO", menuName = "SO/Wave/WaveSO")]
+    public class WaveSO : ScriptableObject
+    {
+        public int waveNum;
+        public float spawnDelay;
+        public float nextWaveTime;
+        public GameObject Enemy;
+        public int enemyLevel;
+
+        //[Header("EnemySpawnPoint")]
+        //public Vector2 minSpawnPoint;
+        //public Vector2 maxSpawnPoint;
+    }
+}
