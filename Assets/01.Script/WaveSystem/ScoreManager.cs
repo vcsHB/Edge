a890ce6de65856fb941edbf6af2491788416bf8d
milkywaycookie <123565749+milkywaycookie@users.noreply.github.com>
2024-12-19 01:17:15 +0900

    feat
    Scoremanager

--- Assets/01.Script/WaveSystem/ScoreManager.cs
+++ Assets/01.Script/WaveSystem/ScoreManager.cs
@@ -2,38 +2,18 @@ using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
-    //public static ScoreManager instance;
-    //private void Awake()
-    //{
-    //    if(instance == null)
-    //    {
-    //        instance = this;
-    //    }
-    //}
-    //public static ScoreManager Instance
-    //{
-    //    get
-    //    {
-    //        if(instance == null)
-    //        {
-    //            return null;
-    //        }
-    //        return instance;
-    //    }
-    //}
-
-    //private int score;
-    //public int Score
-    //{
-    //    get => score;
-    //    set
-    //    {
-    //        score = value;
-    //    }
-    //}
-    //public void PlusScore(int value)
-    //{
-    //    Score += value;
-    //    Debug.Log(score);
-    //}
+    private int score;
+    public int Score
+    {
+        get => score;
+        set
+        {
+            score = value;
+        }
+    }
+    public void PlusScore(int value)
+    {
+        Score += value;
+        Debug.Log(score);
+    }
 }
