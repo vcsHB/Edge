e44c475b890215e0104a998862729e816222a001
bigumdo <showmethe0610@gmail.com>
2024-12-19 23:54:19 +0900

    TitleMenu

--- Assets/01.Script/UI/Title/StartInteraction.cs
+++ Assets/01.Script/UI/Title/StartInteraction.cs
@@ -1,27 +1,47 @@
+using Agents.Players;
+using DG.Tweening;
 using System.Collections;
+using TMPro;
 using UIManage;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace InteractSystem
 {
     public class StartInteraction : InteractObject
     {
-        [SerializeField] private string scneeName;
-        [SerializeField] private UIMovePanel[] movePannel;
+        [SerializeField] private string _scneeName;
+        [SerializeField] private UIMovePanel[] _movePannel;
+        [SerializeField] private GameObject _lineObj;
+        [SerializeField] private TextMeshPro _menuText;
+        [SerializeField] int _baseSize, _selectedFornt;
+        [SerializeField] PlayerMover playerMove;
+
+        private Vector3 _baseScale;
+        private Tween fontSizeTween;
+
 
         private void Awake()
         {
             OnUnDetectedEvent.AddListener(Clear);
             OnDetectedEvent.AddListener(PannelConnection);
+            //_baseScale = transform.localScale;
         }
 
         private void PannelConnection()
         {
             OnInteractEvent.AddListener(OpenPanel);
+            //transform.DOScale(Vector3.one * 1.5f,0.2f).SetEase(Ease.InOutSine);
+            fontSizeTween = DOTween.To(() => _menuText.fontSize, x => _menuText.fontSize = x, _selectedFornt, 0.5f)
+                                .SetEase(Ease.OutExpo);
         }
 
         private void Clear()
         {
+            fontSizeTween.Kill();
+            fontSizeTween = DOTween.To(() => _menuText.fontSize, x => _menuText.fontSize = x, _baseSize, 0.5f)
+                                .SetEase(Ease.OutExpo);
+            //transform.DOScale(_baseScale,0.2f).SetEase(Ease.InOutSine);
             OnInteractEvent.RemoveListener(OpenPanel);
         }
 
@@ -33,16 +53,23 @@ namespace InteractSystem
 
         private void OpenPanel()
         {
-            
+            playerMove.isEdgeMove = false;
+            StartCoroutine(MovePanels());
         }
 
         private IEnumerator MovePanels()
         {
-            for(int i =0;i < movePannel.Length; i++)
+            for(int i =0;i < _movePannel.Length ; i++)
             {
-                movePannel[i].Open();
-                yield return new WaitForSeconds(1f);
+                _movePannel[i].Open();
+                yield return new WaitForSeconds(0.2f);
             }
+
+            _lineObj.transform.DOMoveY(-9,1.5f);
+
+            SceneManager.LoadScene(_scneeName);
+
+
         }
     }
 }
