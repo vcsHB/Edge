97f280c7da1f2a910d8b7830919949d0c3f26411
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-12-19 09:48:53 +0900

    feat: Player Stat Dictionary

--- Assets/01.Script/Core/Manager/ScoreManager.cs
+++ Assets/01.Script/Core/Manager/ScoreManager.cs
@@ -1,4 +1,6 @@
 using System;
+using Agents.Players;
+using StatSystem;
 using UnityEngine;
 namespace Managers
 {
@@ -6,15 +8,39 @@ namespace Managers
     public class ScoreManager : MonoSingleton<ScoreManager>
     {
         public event Action<int> OnScoreChangedEvent;
-        [SerializeField]
-        private int _score;
+        public event Action<int, int> OnFeverChangedEvent;
+        [SerializeField] private Player _player;
+
+        [SerializeField] private int _score;
         public int Score => _score;
+
+        private Stat _scoreBonusStat;
+        private Stat _feverFillMultuipleStat;
+        [SerializeField] private int _maxFeverScore;
+
+        [SerializeField] private int _currentFeverFill;
+
+
+        private void Start()
+        {
+
+            _scoreBonusStat = _player.PlayerStatus.scoreBonus;
+            _feverFillMultuipleStat = _player.PlayerStatus.feverFillMultiple;
+        }
 
 
         public void GainScore(int score)
         {
-            _score += score;
+            int addValue = score + (int)(score * 0.1f * _scoreBonusStat.GetValue());
+            _score += addValue;
+            _currentFeverFill += addValue; 
             OnScoreChangedEvent?.Invoke(_score);
+            if(_currentFeverFill >= _maxFeverScore)
+            {
+                _currentFeverFill = 0;
+                // 실질적인 노리미트 타임을 적용해야함
+            }
+            OnFeverChangedEvent?.Invoke(_currentFeverFill, _maxFeverScore);
         }
     }
 }
