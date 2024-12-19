de7d464c86c4d6a9ea026a31e1a87c4bc36f5178
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-12-19 13:57:54 +0900

    feat: Player Manager In Scene

--- Assets/01.Script/Core/Manager/ScoreManager.cs
+++ Assets/01.Script/Core/Manager/ScoreManager.cs
@@ -35,15 +35,21 @@ namespace Managers
             int addValue = score + (int)(score * 0.1f * _scoreBonusStat.GetValue());
             int addFeverValue = score + (int)(score * 0.1f * _feverFillMultuipleStat.GetValue());
             _score += addValue;
-            _currentFeverFill += addFeverValue; 
+            _currentFeverFill += addFeverValue;
             OnScoreChangedEvent?.Invoke(_score);
-            if(_currentFeverFill >= _maxFeverScore)
+            if (_currentFeverFill >= _maxFeverScore)
             {
                 _currentFeverFill = 0;
                 // 실질적인 노리미트 타임을 적용해야함
-                PlayerManager.Instance.Player.StateMachine.ChangeState("NoLimitIdle");
             }
             OnFeverChangedEvent?.Invoke(_currentFeverFill, _maxFeverScore);
+        }
+
+
+        private void StartNoLimit()
+        {
+            PlayerManager.Instance.Player.StateMachine.ChangeState("NoLimitIdle");
+            VolumeManager.Instance.SetChromatic(1.5f);
         }
     }
 }
